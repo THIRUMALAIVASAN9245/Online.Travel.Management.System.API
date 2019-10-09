bd6e233a217227d7146936d9a81708fe7b5bf127
thirumalaivasan <thirumalaivasan9245@gmail.com>
2019-10-09 16:41:14 +0100

    ok

--- Online.Travel.AuthService.API/Controllers/ValuesController.cs
+++ Online.Travel.AuthService.API/Controllers/ValuesController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Online.Travel.Online.Travel.AuthService.API.Controllers
+namespace Online.Travel.AuthService.API.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
--- Online.Travel.AuthService.API/Startup.cs
+++ Online.Travel.AuthService.API/Startup.cs
@@ -1,31 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-
-namespace Online.Travel.Online.Travel.AuthService.API
+﻿namespace Online.Travel.AuthService.API
 {
-    public class Startup
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Swashbuckle.AspNetCore.Swagger;
+    using global::System.Net.Http;
+    using global::System;
+    using global::System.Reflection;
+    using AutoMapper;
+    using MediatR;
+    using Online.Travel.AuthService.API.Entities;
+    using Online.Travel.AuthService.API.Entities.Repository;
+    using Online.Travel.AuthService.API.Infrastructure;
+    using Online.Travel.AuthService.API.Contract;
+
+    ///<Summary>
+    /// Startup class
+    ///</Summary>
+    public partial class Startup
     {
+        ///<Summary>
+        /// Startup class constructor
+        ///</Summary>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        ///<Summary>
+        /// Configuration
+        ///</Summary>
         public IConfiguration Configuration { get; }
 
+        ///<Summary>
+        /// ConfigureServices method
+        ///</Summary>
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-        }
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
 
+            services.AddMvc();
+            services.AddMediatR();
+
+            services.AddAutoMapper(Assembly.GetAssembly(typeof(ContractMappingProfile)));
+            services.AddScoped<IRepository, Repository>();
+            services.AddScoped<ITokenGenerator, TokenGenerator>();
+            var httpClient = new HttpClient();
+            services.AddSingleton(httpClient);
+
+            string defaultConnectionString = Environment.GetEnvironmentVariable("UserInfoDb") ??
+                                             Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AuthServiceDbContext>(options => options.UseSqlServer(defaultConnectionString));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1.0", new Info
+                {
+                    Version = "v1.0",
+                    Title = "UserInfo API"
+                });
+            });
+        }
+      
+        ///<Summary>
+        /// Configure method
+        ///</Summary>
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -34,7 +84,24 @@ namespace Online.Travel.Online.Travel.AuthService.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseCors("CorsPolicy");
+
+            // MovieCruiser api's ui view 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "UserInfo API v1.0");
+            });
+
+            //app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "api/{controller}/{action}/{id?}");
+            });
         }
     }
 }
+
